b9e332adfa3953dab58715664ef9d80aecfc2435
thomas2110781014 <90864320+thomas2110781014@users.noreply.github.com>
2021-12-04 14:38:01 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
-output "REST API Backend" {
+output "REST_API_Backend" {
   value = ["${aws_instance.ec2-dockerhost.*.public_ip}.nip.io:8000"]
 }
 
-output "PHP Frontend" {
+output "PHP_Frontend" {
   value = ["${aws_instance.ec2-dockerhost.*.public_ip}.nip.io"]
 }
